20f15aa32557b9fb22d86fed3b1039d1c333eb2d
shravani <38989344+shravanipl@users.noreply.github.com>
2023-03-01 19:17:30 -0700

    Create createfiles.sh
    Script for creating 25 files

---
+++ createfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This is a script for creating a batch of 25 files
+
+read -p "Please enter your name for file creation: " name
+if test -z "$name"
+then
+   echo "Name is not entered. Please try again"
+   exit 1
+fi
+
+# Search and extract the highest number from a file 
+startindex=$(ls -lfru | grep $name | tail -n 1 | sed s/$name//)
+test -z $startindex&&startindex=1||startindex 
+
+#Creating a batch of 25 files by appending name and increasing numbers
+for((counter=startindex;counter<startindex+25;counter++));
+do
+        touch $name$counter
+        if [[ $? -eq 0 ]]
+        then
+            echo "File was created $name$counter"
+        else
+            echo "File"$name$counter"was not created"
+        fi
+done
